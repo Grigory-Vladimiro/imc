5fbebb2b1c01581abb94857f742b4ea48d658a4e
Grigory-Vladimiro <grigori.vladimiro@gmail.com>
2025-02-05 23:07:35 +0100

    incorrect_input_test

--- imc/src/test/java/BMICalculatorAppTest.java
+++ imc/src/test/java/BMICalculatorAppTest.java
@@ -2,6 +2,8 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayInputStream;
+
 public class BMICalculatorAppTest {
 
     @Test
@@ -20,5 +22,13 @@ public void testProcessBMI_Obese() {
     public void testProcessBMI_InvalidInput() {
         String result = BMICalculatorApp.processBMI(-10, 1.75);
         assertEquals("Invalid input. Please enter valid numbers.", result);
+    }
+    @Test
+    public void testProcessUserInput_InvalidInput() {
+        String simulatedInput = "abc\nxyz\n"; 
+        System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));
+
+        BMICalculatorApp.processUserInput();
+        assertTrue(true); 
     }
 }
