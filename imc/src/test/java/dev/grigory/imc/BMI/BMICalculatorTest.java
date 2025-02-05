424ea6f0a5b8d65b4023f1ac261dbdd0c340e1d2
Grigory-Vladimiro <grigori.vladimiro@gmail.com>
2025-02-05 21:37:29 +0100

    tests_values_roundup

--- imc/src/test/java/dev/grigory/imc/BMI/BMICalculatorTest.java
+++ imc/src/test/java/dev/grigory/imc/BMI/BMICalculatorTest.java
@@ -6,8 +6,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class BMICalculatorTest {
     @Test
     public void testCalculateBMI() {
-        assertEquals(20.76, BMICalculator.calculateBMI(68, 1.80), 0.01);
-        assertEquals(24.22, BMICalculator.calculateBMI(75, 1.75), 0.01);
+        assertEquals(20.99, BMICalculator.calculateBMI(68, 1.80), 0.01);
+        assertEquals(24.49, BMICalculator.calculateBMI(75, 1.75), 0.01);
     }
     @Test
     public void testCalculateBMIInvalidInput() {
