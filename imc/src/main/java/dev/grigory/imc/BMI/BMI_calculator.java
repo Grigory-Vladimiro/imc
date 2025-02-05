36bbdd62c3fe778bd7f903d074d2f692ff05c691
Grigory-Vladimiro <grigori.vladimiro@gmail.com>
2025-02-05 21:06:51 +0100

    methods_1&2

--- imc/src/main/java/dev/grigory/imc/BMI/BMI_calculator.java
+++ imc/src/main/java/dev/grigory/imc/BMI/BMI_calculator.java
@@ -1,5 +1,29 @@
 package dev.grigory.imc.BMI;
 
 public class BMI_calculator {
-
+    public static double calculateBMI(double weight, double height) {
+        if (weight <= 0 || height <= 0) {
+            throw new IllegalArgumentException("Weight and height must be greater than zero.");
+        }
+        return weight / (height * height);
+    }
+public static String getBMICategory(double bmi) {
+    if (bmi < 16) {
+        return "Severe Thinness";
+    } else if (bmi < 17) {
+        return "Moderate Thinness";
+    } else if (bmi < 18.5) {
+        return "Mild Thinness";
+    } else if (bmi < 25) {
+        return "Normal weight";
+    } else if (bmi < 30) {
+        return "Overweight";
+    } else if (bmi < 35) {
+        return "Obesity class I";
+    } else if (bmi < 40) {
+        return "Obesity class II";
+    } else {
+        return "Obesity class III";
+    }
+}
 }
